ce4fab265c97dc9c990eac8147eb56b0f413fd31
Nail Sharipov <nailxsharipov@gmail.com>
2025-08-09 10:20:05 +0300

    delete empty

--- iCurve/src/int/collision/mod.rs
+++ iCurve/src/int/collision/mod.rs
@@ -3,7 +3,6 @@ pub mod four_convex_path;
 pub mod convex;
 pub(crate) mod x_segment;
 pub mod approximation;
-mod fit;
 mod solver;
 mod pair;
 mod space;
--- iCurve/src/int/collision/space.rs
+++ iCurve/src/int/collision/space.rs
@@ -20,27 +20,6 @@ impl Default for Space {
     }
 }
 
-
-// pub(crate) const FIT_32_MAX: u32 = 28;
-// pub(crate) const FIT_32_ATOM: u32 = 4;
-//
-// pub(crate) trait Fit32 {
-//     fn is_fit32(&self) -> bool;
-//     fn is_atom(&self) -> bool;
-// }
-//
-// impl Fit32 for u32 {
-//     #[inline(always)]
-//     fn is_fit32(&self) -> bool {
-//         *self < FIT_32_MAX
-//     }
-//
-//     #[inline(always)]
-//     fn is_atom(&self) -> bool {
-//         *self < FIT_32_ATOM
-//     }
-// }
-
 impl IntRect {
     #[inline(always)]
     pub(crate) fn max_log_size(&self) -> u32 {
